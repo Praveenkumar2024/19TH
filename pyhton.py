fe2ce09b9baa80602a3cff750678f51251708101
Praveen kumar <130967293+Praveenkumar2024@users.noreply.github.com>
2023-04-19 11:47:43 +0530

    Update pyhton.py

--- pyhton.py
+++ pyhton.py
@@ -4,10 +4,32 @@ for i in range(1,x+1):
          if(i>=j):
             print(i );
      print(end=" ");
+#completed
+
 1
 22
 333
 4444
 555555
          
-       
+ #palindome number
+      num=int(input("entre the number ="))
+      x=num;
+      rev=0;
+      while(num!=0):
+             r=num%10;
+             rev=rev*10+r;
+             num=num/10;
+      if(x==rev):
+           print("it is palindrome")
+      else:
+           print("it iis not pallindrome") 
+  #armstan number
+      num= int(input("enter the number="))
+      sum=0;
+      while(num!=0):
+         r=nnum%10;
+         sum=sum+r*r*r;
+         num=num/10;
+      print(sum);
+      
